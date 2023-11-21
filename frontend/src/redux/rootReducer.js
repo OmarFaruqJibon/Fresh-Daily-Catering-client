a57a786cd4857c2618058b3d6e49eeb0d4ff1654
unknown <jibon602947@gmail.com>
2023-11-21 16:16:51 +0600

    redux init

---
+++ frontend/src/redux/rootReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    loading: false,
+    itemList: []
+}
+
+export const rootReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        default:
+            return state
+    }
+
+}
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,33 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk"
+import { rootReducer } from './rootReducer';
+
+const finalReducer = combineReducers({
+    rootReducer
+});
+
+const initialState = {
+    rootReducer: {
+        cartItems: localStorage.getItem("cartItems")
+            ? JSON.parse(localStorage.getItem("cartItems"))
+            : [],
+
+    }
+}
+const middleware = [thunk]
+
+const store = createStore(
+    finalReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+
+export default store;
+
+
+
+
+
+
